e24f937812a9d5e8305afe41cc0c15813ccd8dbc
artem78 <megabyte1024@ya.ru>
2026-06-11 10:07:54 +0300

    чистка

--- run.sh
+++ run.sh
@@ -4,3 +4,8 @@ docker build -t flask-tilegen .
 docker run -p 5050:5050 flask-tilegen
 
 
+# чистка от расплода контейнеров после каждой пересборки:
+#   docker container ls --all
+#   docker container rm AAA BBB CCC ...
+#   docker rmi $(docker images --filter dangling=true -q)
+
